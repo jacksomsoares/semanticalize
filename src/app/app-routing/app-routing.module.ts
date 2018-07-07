9aff04accd83e23e92c2617b7c932966479d96b2
Guilherme <quiksa@hotmail.com>
2018-07-06 23:50:39 -0300

    ajuste layout

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 
 import { GrammarComponent } from '../grammar/grammar.component';
+import { AnalisePrecedenciaOperadoresComponent } from '../analise-precedencia-operadores/analise-precedencia-operadores.component';
 
-const routes: Routes = [  
-  {path: 'analise-preditiva', component: GrammarComponent }
+const routes: Routes = [
+  { path: 'analise-preditiva', component: GrammarComponent },
+  { path: 'precedencia-operadores', component: AnalisePrecedenciaOperadoresComponent }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   declarations: []
 })
