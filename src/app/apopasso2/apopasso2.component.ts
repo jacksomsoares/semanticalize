8c3b368911c17fccc5e6841b4e455979fc353269
hiltonDias <hilton1995@hotmail.com>
2018-07-08 14:05:54 -0300

    passo 2

--- src/app/apopasso2/apopasso2.component.ts
+++ src/app/apopasso2/apopasso2.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { GrammarV2 } from '../Classes/GrammarV2';
+import { MenorPrecedencia } from '../Classes/menor-precedencia';
+import { OperatorPrecedenceParser } from '../Classes/operator-precedence-parser';
 
 @Component({
   selector: 'app-apopasso2',
@@ -6,10 +9,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apopasso2.component.css']
 })
 export class APOPasso2Component implements OnInit {
-
+  @Input() grammar: string;
+  public tableHeader: string[];
   constructor() { }
+
+  doStepTwo(){
+
+    let grammar = new GrammarV2(this.grammar);
+    let stepTwo = new MenorPrecedencia();
+    let stepOne = new OperatorPrecedenceParser();
+
+    stepOne.FirstStep(grammar);
+
+    stepTwo.findTNT(grammar);
+    this.tableHeader = stepTwo.listaTNT;
+
+    let maior: number = 0;
+
+    for(let x = 0; x < this.tableHeader.length; x++){
+      
+      let a = stepOne.leading.get(this.tableHeader[x].toString().split("")[1]).toString().split(",");
+
+      if(a.length > maior){
+        maior = a.length;
+      }
+
+    }
+
+    for(let x = 0; x < this.tableHeader.length; x++){
+
+      let precedencia = this.tableHeader[x].toString().split("");
+      
+      let letra = stepOne.leading.get(precedencia[1]).toString().split(",");
+
+      for(let j = 0; j < letra.length; j++){
+        // let precedencias = letra[j].toString().split("");
+        console.log(letra[j]);
+      }
+
+      //Mostrar a tabela deitada
+
+    }
+
+    
+
+
+    
+  }
+
+  public getMaior(){
+
+    
+    
+}
 
   ngOnInit() {
   }
 
 }
+
+
