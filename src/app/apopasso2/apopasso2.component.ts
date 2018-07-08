a2fc06e06a3345e00cb2a7751bf97f1955a698ac
Guilherme <quiksa@hotmail.com>
2018-07-08 20:38:55 -0300

    menor precedencia

--- src/app/apopasso2/apopasso2.component.ts
+++ src/app/apopasso2/apopasso2.component.ts
@@ -11,10 +11,13 @@ import { OperatorPrecedenceParser } from '../Classes/operator-precedence-parser'
 export class APOPasso2Component implements OnInit {
   @Input() grammar: string;
   public tableHeader: string[];
-  public tableRow: string[];
-  constructor() { }
+  public tableRow: Array<Array<string>>;
+  public lineprecedencia: Map<String, Array<string>>;
+  constructor() {
+    this.lineprecedencia = new Map<String, Array<string>>;
+  }
 
-  doStepTwo(){
+  doStepTwo() {
 
     let grammar = new GrammarV2(this.grammar);
     let stepTwo = new MenorPrecedencia();
@@ -28,47 +31,67 @@ export class APOPasso2Component implements OnInit {
 
     let maior: number = 0;
 
-    for(let x = 0; x < this.tableHeader.length; x++){
-      
+    for (let x = 0; x < this.tableHeader.length; x++) {
+
       let a = stepOne.leading.get(this.tableHeader[x].toString().split("")[1]).toString().split(",");
 
-      if(a.length > maior){
+      if (a.length > maior) {
         maior = a.length;
       }
 
     }
 
-    for(let x = 0; x < this.tableHeader.length; x++){
+    // let temp1 = new Array<string>;
+    // temp1.push("/<&");
+    // temp1.push("/<(");
+    // temp1.push("/<id");
+    // this.lineprecedencia.set('T', temp1);
 
+    // let temp2 = new Array<string>;
+    // temp2.push("& < (");
+    // temp2.push("& < id");
+    // this.lineprecedencia.set('F', temp2);
+
+    // let temp3 = new Array<string>;
+    // temp3.push("( < /");
+    // temp3.push("( < & ");
+    // temp3.push("( < (");
+    // temp3.push("( < id");
+    // this.lineprecedencia.set('E', temp3);
+
+    for (let x = 0; x < this.tableHeader.length; x++) {
       let precedencia = this.tableHeader[x].toString().split("");
-      
+      let temp3 = new Array<string>;
       let letra = stepOne.leading.get(precedencia[1]).toString();
-      
-      let stringLinha = this.tableHeader[x].toString() + " -> " + precedencia[0].toString() + "<" + letra.replace(/,/g,", "+precedencia[0].toString() + "<") 
-      console.log(stringLinha);
-      this.tableRow.push(stringLinha);
 
-      for(let j = 0; j < letra.length; j++){
-        
-        // let precedencias = letra[j].toString().split("");  
-        // console.log(letra[j]);
+      const array = letra.split(",");
+      for (let x = 0; x < array.length; x++) {
+        const string_aux = precedencia[0].toString() + "<" + array[x];
+        temp3.push(string_aux);
       }
-
-      //Mostrar a tabela deitada
+      this.lineprecedencia.set(precedencia[1], temp3);
 
     }
 
-    
-
-
-    
+    for (let x = 0; x < this.tableHeader.length; x++) {
+      let precedencia = this.tableHeader[x].toString().split("");
+      let tableLine = [];
+      this.lineprecedencia.forEach((value, key) => {
+        if (precedencia[1] === key) {
+          for (let x = 0; x < value.length; x++) {
+            tableLine.push(value[x]);
+          }
+        }
+      });
+      this.tableRow.push(tableLine);
+    }
   }
 
-  public getMaior(){
+  public getMaior() {
 
-    
-    
-}
+
+
+  }
 
   ngOnInit() {
   }
