0b1189db2695126ceec2a89b3deb8b626e13778d
Guilherme <quiksa@hotmail.com>
2018-07-10 02:20:44 -0300

    ajutes

--- src/app/apopasso2/apopasso2.component.ts
+++ src/app/apopasso2/apopasso2.component.ts
@@ -3,6 +3,7 @@ import { GrammarV2 } from '../Classes/GrammarV2';
 import { MenorPrecedencia } from '../Classes/menor-precedencia';
 import { OperatorPrecedenceParser } from '../Classes/operator-precedence-parser';
 import { MaiorPrecedencia } from '../Classes/maior-precedencia';
+import { Table } from '../Classes/Table';
 
 @Component({
   selector: 'app-apopasso2',
@@ -37,70 +38,18 @@ export class APOPasso2Component implements OnInit {
   }
 
   doCreateTable() {
-    let grammar = new GrammarV2(this.grammar);
-    let stepTwo = new MenorPrecedencia();
-    let stepOne = new OperatorPrecedenceParser();
-    stepOne.FirstStep(grammar);
+    const tb = new Table();
+    const grammar = new GrammarV2(this.grammar);
+    // let stepTwo = new MenorPrecedencia();
+    // let stepOne = new OperatorPrecedenceParser();
+    // let stepTree = new MaiorPrecedencia();
+    // stepOne.FirstStep(grammar);
 
-    for (let x = 0; x < grammar.terminais.length; x++) {
-      const element = grammar.terminais[x];
-      this.tableHead.push(element);
-    }
-    this.tableHead.push('$');
-
-    // menor precedencia
-    this.menorprecedencia.forEach((value, key) => {
-      for (let x = 0; x < value.length; x++) {
-        this.aux.push(value[x]);
-      }
-    });
-    // maior precedencia
-    this.maiorprecedencia.forEach((value, key) => {
-      for (let x = 0; x < value.length; x++) {
-        this.aux.push(value[x]);
-      }
-    });
-
-    // marcador menor
-    for (let x = 0; x < this.tableRowMenorMarcador.length; x++) {
-      const element = this.tableRowMenorMarcador[x];
-      this.aux.push(element.toString());
-    }
-    // marcador maior
-    for (let y = 0; y < this.tableRowMaiorMarcador.length; y++) {
-      const element = this.tableRowMaiorMarcador[y];
-      this.aux.push(element.toString());
-    }
-
-    for (let i = 0; i < this.tableHead.length; i++) {
-      const tableLine = [];
-      const i_item = this.tableHead[i];
-      tableLine.push(i_item);
-      for (let j = 0; j < this.tableHead.length; j++) {
-        const j_item = this.tableHead[j];
-        let achou = false;
-        for (let index = 0; index < this.aux.length; index++) {
-          const aux_element = this.aux[index];
-          const aux1 = i_item + ' < ' + j_item;
-          const aux2 = i_item + ' > ' + j_item;
-          if (aux_element === aux1) {
-            tableLine.push('<');
-            this.aux.splice(index, 1);
-            index = this.aux.length - 1;
-            achou = true;
-          } else if (aux_element === aux2) {
-            tableLine.push('>');
-            this.aux.splice(index, 1);
-            index = this.aux.length - 1;
-            achou = true;
-          }
-        }
-        if (!achou) {
-          tableLine.push('');
-        }
-      }
-      this.tableRow.push(tableLine);
-    }
+    this.tableHead = tb.getHeaderTable(grammar);
+    let table = new Map<string, Map<string, string>>();
+    //table = tb.getMapTable(this.grammar);
+    this.tableRow = tb.getRowTable(this.grammar);
+    table = tb.getMapTable(this.grammar);
 
   }
 
@@ -108,35 +57,14 @@ export class APOPasso2Component implements OnInit {
 
     let grammar = new GrammarV2(this.grammar);
     let stepTwo = new MenorPrecedencia();
-    let stepOne = new OperatorPrecedenceParser();
-
-    stepOne.FirstStep(grammar);
-
     stepTwo.findTNT(grammar);
+
     this.tableHeaderP2 = stepTwo.listaTNT;
     this.tableRowP2 = [];
     this.tableRowMenorMarcador = [];
 
-    for (let x = 0; x < this.tableHeaderP2.length; x++) {
-      let precedencia = this.tableHeaderP2[x].toString().split("");
-      let precedence_aux = this.tableHeaderP2[x];
-      let temp3 = new Array<string>();
-      let letra = stepOne.leading.get(precedencia[1]).toString();
-
-      const array = letra.split(',');
-      for (let y = 0; y < array.length; y++) {
-        if (x === this.tableHeaderP2.length - 1) { //save precedencia marcador menor
-          const str = new Array<string>();
-          str.push('$' + ' < ' + array[y]);
-          this.tableRowMenorMarcador.push(str);
-        }
-
-        const string_aux = precedencia[0].toString() + ' < ' + array[y];
-        temp3.push(string_aux);
-      }
-      this.menorprecedencia.set(precedence_aux, temp3);
-
-    }
+    this.menorprecedencia = stepTwo.getMenorPrecedencia(this.grammar);
+    this.tableRowMenorMarcador = stepTwo.getTableRowMenorMarcador(this.grammar);
 
     for (let x = 0; x < this.tableHeaderP2.length; x++) {
       let precedencia = this.tableHeaderP2[x];
@@ -157,37 +85,13 @@ export class APOPasso2Component implements OnInit {
 
     let grammar = new GrammarV2(this.grammar);
     let stepTree = new MaiorPrecedencia();
-    let stepOne = new OperatorPrecedenceParser();
-
-    stepOne.FirstStep(grammar);
-
     stepTree.findNTT(grammar);
     this.tableHeaderP3 = stepTree.listaNTT;
     this.tableRowP3 = [];
     this.tableRowMaiorMarcador = [];
 
-
-
-    for (let x = 0; x < this.tableHeaderP3.length; x++) {
-      let precedencia = this.tableHeaderP3[x].toString().split("");
-      let precedence_aux = this.tableHeaderP3[x];
-
-      let temp3 = new Array<string>();
-      let letra = stepOne.trailing.get(precedencia[0]).toString();
-
-      const array = letra.split(',');
-      for (let y = 0; y < array.length; y++) {
-        if (x === 0) { //save precedencia marcador maior
-          const str = new Array<string>();
-          str.push(array[y] + ' > ' + '$');
-          this.tableRowMaiorMarcador.push(str);
-        }
-        const string_aux = array[y] + ' > ' + precedencia[1];
-        temp3.push(string_aux);
-      }
-      this.maiorprecedencia.set(precedence_aux, temp3);
-
-    }
+    this.maiorprecedencia = stepTree.getMaiorPrecedencia(this.grammar);
+    this.tableRowMaiorMarcador = stepTree.getTableRowMaiorMarcador(this.grammar);
 
     for (let x = 0; x < this.tableHeaderP3.length; x++) {
       let precedencia = this.tableHeaderP3[x];
