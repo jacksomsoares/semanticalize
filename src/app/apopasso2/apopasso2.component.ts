88119ce62d0d2dacf83cf8925063002d6bb62b85
Guilherme <quiksa@hotmail.com>
2018-07-09 22:10:22 -0300

    tabela

--- src/app/apopasso2/apopasso2.component.ts
+++ src/app/apopasso2/apopasso2.component.ts
@@ -13,20 +13,95 @@ export class APOPasso2Component implements OnInit {
   @Input() grammar: string;
   public tableHeaderP2: string[];
   public tableHeaderP3: string[];
+  public aux: string[];
+  public tableHead: Array<string>;
   public tableRowP2: Array<Array<string>>;
   public tableRowP3: Array<Array<string>>;
   public tableRowMaiorMarcador: Array<Array<string>>;
   public tableRowMenorMarcador: Array<Array<string>>;
+  public tableRow: Array<Array<string>>;
   public menorprecedencia: Map<String, Array<string>>;
   public maiorprecedencia: Map<String, Array<string>>;
   constructor() {
     this.menorprecedencia = new Map<String, Array<string>>();
     this.maiorprecedencia = new Map<String, Array<string>>();
+    this.tableHead = [];
+    this.tableRow = [];
+    this.aux = [];
   }
 
   doLoad() {
     this.doStepTwo();
     this.doStepTree();
+    this.doCreateTable();
+  }
+
+  doCreateTable() {
+    let grammar = new GrammarV2(this.grammar);
+    let stepTwo = new MenorPrecedencia();
+    let stepOne = new OperatorPrecedenceParser();
+    stepOne.FirstStep(grammar);
+
+    for (let x = 0; x < grammar.terminais.length; x++) {
+      const element = grammar.terminais[x];
+      this.tableHead.push(element);
+    }
+    this.tableHead.push('$');
+
+    // menor precedencia
+    this.menorprecedencia.forEach((value, key) => {
+      for (let x = 0; x < value.length; x++) {
+        this.aux.push(value[x]);
+      }
+    });
+    // maior precedencia
+    this.maiorprecedencia.forEach((value, key) => {
+      for (let x = 0; x < value.length; x++) {
+        this.aux.push(value[x]);
+      }
+    });
+
+    // marcador menor
+    for (let x = 0; x < this.tableRowMenorMarcador.length; x++) {
+      const element = this.tableRowMenorMarcador[x];
+      this.aux.push(element.toString());
+    }
+    // marcador maior
+    for (let y = 0; y < this.tableRowMaiorMarcador.length; y++) {
+      const element = this.tableRowMaiorMarcador[y];
+      this.aux.push(element.toString());
+    }
+
+    for (let i = 0; i < this.tableHead.length; i++) {
+      const tableLine = [];
+      const i_item = this.tableHead[i];
+      tableLine.push(i_item);
+      for (let j = 0; j < this.tableHead.length; j++) {
+        const j_item = this.tableHead[j];
+        let achou = false;
+        for (let index = 0; index < this.aux.length; index++) {
+          const aux_element = this.aux[index];
+          const aux1 = i_item + ' < ' + j_item;
+          const aux2 = i_item + ' > ' + j_item;
+          if (aux_element === aux1) {
+            tableLine.push('<');
+            this.aux.splice(index, 1);
+            index = this.aux.length - 1;
+            achou = true;
+          } else if (aux_element === aux2) {
+            tableLine.push('>');
+            this.aux.splice(index, 1);
+            index = this.aux.length - 1;
+            achou = true;
+          }
+        }
+        if (!achou) {
+          tableLine.push('');
+        }
+      }
+      this.tableRow.push(tableLine);
+    }
+
   }
 
   doStepTwo() {
